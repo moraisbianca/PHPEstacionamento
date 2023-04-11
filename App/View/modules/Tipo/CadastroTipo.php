aec93b5a3fecc739117306fc5e9710a44b32a460
moraisbianca <biancacamilamorais@gmail.com>
2023-04-11 15:59:37 -0300

    TESTANDO MENU (APLICAR DROPDOWN EM TODOS)

--- App/View/modules/Tipo/CadastroTipo.php
+++ App/View/modules/Tipo/CadastroTipo.php
@@ -21,8 +21,15 @@
 
 <div class="container">
     <ul class="nav nav-tabs">
-        <li class="nav-item">
-            <a class="nav-link" href="/veiculo/form">Veículo</a>
+        <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Veículo
+            </a>
+            <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="/veiculo/form">Cadastrar</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="/veiculo">Visualizar</a></li>
+          </ul>
         </li>
         <li class="nav-item">
             <a class="nav-link active" aria-current="page" href="">Tipo</a>
@@ -37,18 +44,17 @@
             <a class="nav-link" href="/combustivel/form">Combustível</a>
         </li>
 
-
+        <!--
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
             Dropdown
           </a>
           <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li><a class="dropdown-item" href="/veiculo/form">Cadastrar</a></li>
             <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="#">a</a></li>
+            <li><a class="dropdown-item" href="/veiculo">Visualizar</a></li>
           </ul>
-        </li>
+        </li> !-->
 
     </ul>
 </div>
