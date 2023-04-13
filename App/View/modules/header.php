848d672b8f1f47b29db2977afa5d48c7c5ea2e31
Gabe_Tesser <95052551+GabrielTesser@users.noreply.github.com>
2023-04-13 15:51:49 -0300

    Add files via upload

---
+++ App/View/modules/header.php
@@ -0,0 +1,45 @@
+<header>
+
+<div class="container">
+    <ul class="nav nav-tabs">
+        <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Veículo
+            </a>
+            <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="/veiculo/form">Cadastrar</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="/veiculo">Visualizar</a></li>
+          </ul>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" aria-current="page" href="">Tipo</a>
+            <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="/tipo/form">Cadastrar</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="/tipo">Visualizar</a></li>
+        </li>
+        <li class="nav-item">
+        <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="">Marca</a> 
+         <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="/marca/form">Cadastrar</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="/marca">Visualizar</a></li>
+        </li>
+        <li class="nav-item">
+        <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Fabricante</a>
+        <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="/fabricante/form">Cadastrar</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="/fabricante">Visualizar</a></li>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" >Combustível</a>
+            <li><a class="dropdown-item" href="/combustivel/form">Cadastrar</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="/combustivel">Visualizar</a></li>
+        </li>
+
+    </ul>
+</div>
+</header>
